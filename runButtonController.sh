f11f3f883ec2e17dc419d51eff58dd8d5b4774c0
Justin Miller <justinmiller24@gmail.com>
2022-08-21 22:04:01 -0500

    Committing new files

---
+++ runButtonController.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script to make sure button controller script is running
+# Created by Justin Miller on 8.21.2022
+
+
+# Check if running
+if pgrep runButtonController.py >/dev/null 2>&1
+then
+    # Script is running, nothing to do
+    exit
+
+else
+    # Script is not running, start script
+    python buttonController.py >> /home/fpp/media/logs/runButtonController.log 2>&1 &
+
+fi
+
+

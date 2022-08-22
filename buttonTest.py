ec1b2309aaffce814cd8454fe062f676fd36261e
Justin Miller <justinmiller24@gmail.com>
2022-08-21 20:52:24 -0500

    Update button script

--- buttonTest.py
+++ buttonTest.py
@@ -1,7 +1,6 @@
 # Python script to handle I2C button input and output
 # Created by Justin Miller on 8.21.2022
 
-
 # Install packages (bash)
 # sudo apt-get update
 # sudo apt-get upgrade
@@ -14,14 +13,15 @@ import board
 import digitalio
 from adafruit_seesaw.seesaw import Seesaw
 from adafruit_seesaw.digitalio import DigitalIO
-from adafruit_seesaw.pwmout import PWMOut
+#from adafruit_seesaw.pwmout import PWMOut
 
 # Initialize I2C
 import busio
 i2c = busio.I2C(board.SCL, board.SDA)
 qt = Seesaw(i2c, addr=0x3A)
 
-# Buttonsi
+# Buttons
+BUTTON_COLORS = ("RED", "WHITE", "GREEN", "BLUE")
 BUTTON_PINS = (18, 19, 20, 2)
 btns = []
 for btnPin in BUTTON_PINS:
@@ -32,25 +32,23 @@ for btnPin in BUTTON_PINS:
 
 # LEDs
 LED_PINS = (12, 13, 0, 1)
-leds = []
-for ledPin in LED_PINS:
-    led = PWMOut(qt, ledPin)
-    leds.append(led)
 
-# Loop
-while True:
-    for ledNumber, button in enumerate(btns):
-        if not button.value:
-            qt.digital_write(LED_PINS[ledNumber], True)
-        else:
-            qt.digital_write(LED_PINS[ledNumber], False)
 
-#            for cycle in range(0, 65535, 8000):
-#                leds[ledNumber].duty_cycle = cycle
-#                time.sleep(0.01)
-#            for cycle in range(65534, 0, -8000):
-#                leds[ledNumber].duty_cycle = cycle
-#                time.sleep(0.01)
+try:
+    while True:
+        for ledNumber, button in enumerate(btns):
+            if not button.value:
+                print("Button pushed:", BUTTON_PINS[ledNumber], BUTTON_COLORS[ledNumber])
+                print("Light color:", LED_PINS[ledNumber], BUTTON_COLORS[ledNumber])
+#            qt.digital_write(LED_PINS[ledNumber], True)
+
+except KeyboardInterrupt:
+    pass
+
+
+print("Stopping...")
+exit()
+
 
 
 
